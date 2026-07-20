792400bc7ad814f6950119ec9a85dde5744707ea
董全帅 <keephappyboy>
2026-07-20 19:03:42 +0800

    Initial AuroraAscent blog

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'AuroraAscent';
+export const SITE_DESCRIPTION = '记录学习、构建与持续向上的个人空间。';
